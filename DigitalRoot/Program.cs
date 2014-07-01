5541584c55fd60defc0184e6edf703381a06a6ba
Zach Urbach <urbach-zach@gmail.com>
2014-07-01 16:45:39 -0600

    digital root of a number

---
+++ DigitalRoot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+        static void DigitalRoot(string rootThis)
+        {
+
+        }
+    }
+}
