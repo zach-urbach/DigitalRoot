6c101d96de8119b144285bf1743284f2c5987964
Zach Urbach <urbach-zach@gmail.com>
2014-07-01 23:03:42 -0600

    not done

--- DigitalRoot/Program.cs
+++ DigitalRoot/Program.cs
@@ -10,14 +10,47 @@ namespace DigitalRoot
     {
         static void Main(string[] args)
         {
+            DigitalRoot("45734");
 
-
-
+            Console.ReadKey();
         }
 
-        static void DigitalRoot(string rootThis)
+        static int DigitalRoot(string rootThis)
         {
+            
+            List<int> digits = new List<int>();
+            
+            string currentNum = rootThis;
 
+            while (currentNum.Length > 1)
+            {
+                
+                int counter = 0;
+
+                
+                for (int i = 0; i < currentNum.Length; i++)
+                {
+                    
+                    digits.Add(Convert.ToInt32(currentNum[i].ToString()));
+                }
+                
+                foreach (var item in digits)
+                {
+                    
+                    counter += item;
+                }
+                
+                currentNum = counter.ToString();
+                
+                digits.Clear();
+            }
+
+            int digitalRoot = Convert.ToInt32(currentNum);
+
+            Console.WriteLine("Input: " + rootThis);
+            Console.WriteLine("Output: " + digitalRoot);
+
+            return digitalRoot;
         }
     }
 }
